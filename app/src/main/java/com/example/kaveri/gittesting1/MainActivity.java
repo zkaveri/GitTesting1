6dfcb8f3d2de5adb8cc483a48854769eb4fd1f0f
kaveri <kaverizaware@gmail.com>
2018-05-13 11:57:55 +0530

    View initialization

--- app/src/main/java/com/example/kaveri/gittesting1/MainActivity.java
+++ app/src/main/java/com/example/kaveri/gittesting1/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.kaveri.gittesting1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private TextView textTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //done
+        textTitle = (TextView) findViewById(R.id.text_title);
+        textTitle.setText("Git integration testing");
     }
 }
